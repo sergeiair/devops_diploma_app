308bea0c18e4da556a5561bd05e3e4231353142e
Siarhei Zhardzetski <zsggem@gmail.com>
2025-01-24 00:37:17 +0100

    upd

--- app/HelloWorldServer.java
+++ app/HelloWorldServer.java
@@ -5,31 +5,32 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.time.Instant;
-import java.util.Arrays;
 
 public class HelloWorldServer {
 
-    private final static String buildMade = getDateString();
+    private final static String buildMade = getDateTimeString();
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
         server.createContext("/", new HelloHandler());
         server.setExecutor(null);
-        System.out.println("Server started on port 8080...");
         server.start();
     }
 
     static class HelloHandler implements HttpHandler {
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hi from Java at " + getDateString() + "!! \r\n \r\n Build made at " + buildMade;
-            exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
+            String response = "Hey from Java App at " + getDateTimeString() + "! \r\n \r\n App started at " + buildMade;
+
+            exchange.sendResponseHeaders(200, response.length());
+
             os.write(response.getBytes());
             os.close();
         }
     }
 
-    public static String getDateString() {
+    public static String getDateTimeString() {
         return Instant.now().toString().split("\\.")[0];
     }
 }
